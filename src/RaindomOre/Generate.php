779856c9d88a9a3af2153d501199a94570c101c6
BlueGamesNetworkPMMP <140811591+Terpzz@users.noreply.github.com>
2023-10-10 01:31:20 -0700

    Update Generate.php

--- src/RaindomOre/Generate.php
+++ src/RaindomOre/Generate.php
@@ -23,45 +23,46 @@ class Generate extends PluginBase implements Listener {
     }
 
     public function onBlockSet(BlockUpdateEvent $event) {
-        $block = $event->getBlock();
-        $water = false;
-        $lava = false;
-        for ($i = 2; $i <= 5; $i++) {
-            $nearBlock = $block->getSide($i);
-            if ($nearBlock instanceof Water) {
-                $water = true;
-            } elseif ($nearBlock instanceof Lava) {
-                $lava = true;
+    $block = $event->getBlock();
+    $water = false;
+    $lava = false;
+    for ($i = 2; $i <= 5; $i++) {
+        $nearBlock = $block->getSide($i);
+        if ($nearBlock instanceof Water) {
+            $water = true;
+        } elseif ($nearBlock instanceof Lava) {
+            $lava = true;
+        }
+        if ($water && $lava) {
+            $id = mt_rand(1, 20);
+            switch ($id) {
+                case 2:
+                    $newBlock = VanillaBlocks::IRON_ORE();
+                    break;
+                case 4:
+                    $newBlock = VanillaBlocks::GOLD_ORE();
+                    break;
+                case 6:
+                    $newBlock = VanillaBlocks::EMERALD_ORE();
+                    break;
+                case 8:
+                    $newBlock = VanillaBlocks::COAL_ORE();
+                    break;
+                case 10:
+                    $newBlock = VanillaBlocks::REDSTONE_ORE();
+                    break;
+                case 12:
+                    $newBlock = VanillaBlocks::DIAMOND_ORE();
+                    break;
+                case 14:
+                    $newBlock = VanillaBlocks::LAPIS_LAZULI_ORE();
+                    break;
+                default:
+                    $newBlock = VanillaBlocks::COBBLESTONE();
             }
-            if ($water && $lava) {
-                $id = mt_rand(1, 20);
-                switch ($id) {
-                    case 2:
-                        $newBlock = VanillaBlocks::IRON_ORE();
-                        break;
-                    case 4:
-                        $newBlock = VanillaBlocks::GOLD_ORE();
-                        break;
-                    case 6:
-                        $newBlock = VanillaBlocks::EMERALD_ORE();
-                        break;
-                    case 8:
-                        $newBlock = VanillaBlocks::COAL_ORE();
-                        break;
-                    case 10:
-                        $newBlock = VanillaBlocks::REDSTONE_ORE();
-                        break;
-                    case 12:
-                        $newBlock = VanillaBlocks::DIAMOND_ORE();
-                        break;
-                    case 14:
-                        $newBlock = VanillaBlocks::LAPIS_LAZULI_ORE();
-                        break;
-                    default:
-                        $newBlock = VanillaBlocks::COBBLESTONE();
-                }
-                $event->getBlock()->getWorld()->setBlock($block->getPosition(), $newBlock);
-                return;
+            $world = $block->getWorld();
+            $world->setBlock($block->getPosition(), $newBlock);
+            return;
             }
         }
     }
